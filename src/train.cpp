deaf2f32a98ce19ee66e739cdd6de3a62bb3d42c
Shaybye <159885796+Shaybye@users.noreply.github.com>
2024-05-10 18:40:27 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,56 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+
+Train::Train()
+{
+    countOp = 0;
+    first = nullptr;
+}
+void Train::addCage(bool light)
+{
+    Cage* cage = new Cage;
+    cage->light = light;
+    cage->next = nullptr;
+    cage->prev = nullptr;
+    if (first == nullptr) {
+        first = cage;
+        first->next = first;
+        first->prev = first;
+    } else {
+        Cage* current = first;
+        while (current->next != first) {
+            current = current->next;
+        }
+        current->next = cage;
+        cage->prev = current;
+        cage->next = first;
+        first->prev = cage;
+    }
+}
+int Train::getLength()
+{
+    int curLen = 0;
+    Cage* current = first;
+    current->light = true;
+    while (true) {
+        current = current->next;
+        countOp++;
+        curLen++;
+        if (current->light) {
+            current->light = false;
+            int finLen = curLen;
+            for (int i = 0; i < finLen; ++i) {
+                current = current->prev;
+                countOp++;
+                curLen--;
+            }
+            if (!current->light)
+                return finLen;
+
+        }
+    }
+}
+int Train::getOpCount()
+{
+    return countOp;
+}
